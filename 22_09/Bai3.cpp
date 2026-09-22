29bb239f136db72af5e6841b37787a69501870d1
KidoKosho <152004626+KidoKosho@users.noreply.github.com>
2026-09-23 00:08:02 +0700

    push bai 3

--- 22_09/Bai3.cpp
+++ 22_09/Bai3.cpp
@@ -1,20 +1,36 @@
-#include <bits/stdc++.h>
-
+#include<iostream>
+#include<cmath>
 using namespace std;
-
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+int main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
     int n;
     cin >> n;
-    long long tong = 0,solve = LLONG_MIN,min_array =0;
-    for(int i=0;i<n;++i){
-        int x;
+    long long tong = 0;
+    int a[n];
+    for(auto &x : a) {
         cin >> x;
         tong += x;
-        solve = max(tong-min_array,solve);
-        min_array = min(tong,min_array);
     }
-    cout << solve;
-    return 0;
+    unsigned long sum1 =0,sum2=0;
+    for(int i=0;i<n;++i){
+        if(a[i] %2 ==0){
+            unsigned long long p =0;
+            int k =a[i];
+            while(k%2==0){
+                p++;
+                k/=2;
+            }
+            sum1 += p*(tong - a[i]);
+        }
+        if(a[i]%5==0){
+            unsigned long long p =0;
+            int k = a[i];
+            while(k%5==0){
+                p++;
+                k/=5;
+            }
+            sum2 += p*(tong - a[i]);
+        }
+    }
+    cout << min(sum1,sum2);
 }
